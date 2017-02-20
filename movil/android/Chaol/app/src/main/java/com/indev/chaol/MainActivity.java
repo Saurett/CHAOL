224c90182941bf7ddd7d61f87145071a771469f5
Saurett <javiersaurett@gmail.com>
2017-02-20 16:46:15 -0600

    Android > Versión 1.0.2
    -Se agregan iconos para el navigation drawer

--- movil/android/Chaol/app/src/main/java/com/indev/chaol/MainActivity.java
+++ movil/android/Chaol/app/src/main/java/com/indev/chaol/MainActivity.java
@@ -1,13 +1,54 @@
 package com.indev.chaol;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText txtUsername, txtPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Se inicializan los compotentes a utilizar
+        btnLogin = (Button) findViewById(R.id.btn_login);
+
+        //Se asignar "this" para direccionar al evento onClick de View.OnClickListener
+        btnLogin.setOnClickListener(this);
+
+    }
+
+    //Metodo implementado del View.OnClickListener
+    @Override
+    public void onClick(View view) {
+
+        //View.OnClickListenerView.OnClickListener
+        int id = view.getId();
+
+        switch (id) {
+            case R.id.btn_login:
+                validationLogin();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    //Valida los elementos del login
+    public void validationLogin() {
+        openNavigation();
+    }
+
+    private void openNavigation() {
+        Intent intent = new Intent(this, NavigationDrawer.class);
+        startActivity(intent);
     }
 }
