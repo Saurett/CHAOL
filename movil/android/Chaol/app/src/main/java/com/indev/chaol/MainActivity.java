81ec694536491f45d604492722ffe9992ca2ead7
Saurett <javiersaurett@gmail.com>
2017-02-27 22:01:11 -0600

    Android > Versión 1.0.8
    - Se agrega acciones de recuperar contraseña

--- movil/android/Chaol/app/src/main/java/com/indev/chaol/MainActivity.java
+++ movil/android/Chaol/app/src/main/java/com/indev/chaol/MainActivity.java
@@ -4,15 +4,20 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText txtUsername, txtPassword;
-    private Button btnLogin, btnRegister, btnForgotPassword;
+    private TextInputEditText txtUsername, txtPassword, txtEmail;
+    private Button btnLogin, btnRegister, btnForgotPassword, btnBack, btnSendEmail;
+    private LinearLayout formForgot, formLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +28,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
 
         /**Se inicializan los compotentes a utilizar**/
+        formForgot = (LinearLayout) findViewById(R.id.form_forgot);
+        formLogin = (LinearLayout) findViewById(R.id.form_login);
+
+        txtUsername = (TextInputEditText) findViewById(R.id.txt_login_username);
+        txtPassword = (TextInputEditText) findViewById(R.id.txt_login_password);
+        txtEmail = (TextInputEditText) findViewById(R.id.txt_login_email);
+
         btnLogin = (Button) findViewById(R.id.btn_login);
         btnRegister = (Button) findViewById(R.id.btn_register);
         btnForgotPassword = (Button) findViewById(R.id.btn_forgot_password);
+        btnSendEmail = (Button) findViewById(R.id.btn_send_email);
+        btnBack = (Button) findViewById(R.id.btn_back_login);
 
-        this.setStyles();
+        /**se cargan estilos**/
+        this.onPreRender();
 
         /**Se asignar "this" para direccionar al evento onClick de View.OnClickListener**/
         btnLogin.setOnClickListener(this);
         btnRegister.setOnClickListener(this);
+        btnForgotPassword.setOnClickListener(this);
+        btnSendEmail.setOnClickListener(this);
+        btnBack.setOnClickListener(this);
     }
 
-    /**Metodo implementado del View.OnClickListener**/
+    /**
+     * Metodo implementado del View.OnClickListener
+     **/
     @Override
     public void onClick(View view) {
         int id = view.getId();
@@ -46,17 +66,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_register:
                 openRegister();
                 break;
+            case R.id.btn_forgot_password:
+            case R.id.btn_back_login:
+                showLoginActions();
+                break;
             default:
                 break;
         }
     }
 
-    /**Valida los elementos del login**/
+    /**
+     * Valida los elementos del login
+     **/
     public void validationLogin() {
         openNavigation();
     }
 
-    /**Inicia el NavigationDrawerActivity y sale del login**/
+    /**
+     * Muestra y oculta componentes dependiendo la accion
+     **/
+    public void showLoginActions() {
+
+        formLogin.setVisibility((formLogin.getVisibility() == View.VISIBLE) ? View.GONE : View.VISIBLE);
+        formForgot.setVisibility((formForgot.getVisibility() == View.GONE) ? View.VISIBLE : View.GONE);
+    }
+
+    /**
+     * Inicia el NavigationDrawerActivity y sale del login
+     **/
     private void openNavigation() {
         Intent intent = new Intent(this, NavigationDrawerActivity.class);
         startActivity(intent);
@@ -67,8 +104,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivity(intent);
     }
 
-    /**Asiga el estilo de subrayado al texto del boton**/
-    private void setStyles() {
+    /**
+     * Asiga el estilo de subrayado al texto del boton
+     **/
+    private void onPreRender() {
         btnForgotPassword.setPaintFlags(btnForgotPassword.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 }
