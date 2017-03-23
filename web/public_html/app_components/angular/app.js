9f85b58ee167425553f53fcb8e2ebe344494d083
FredGz <fredleyva@gmail.com>
2017-03-22 20:50:08 -0600

    Implementación de maquetación 22/03
    * Maquetación de ventanas restantes de Administración
    * Maquetación de ventana de Perfil.
    * Maquetación de ventana Fletes.

--- web/public_html/app_components/angular/app.js
+++ web/public_html/app_components/angular/app.js
@@ -20,5 +20,56 @@ app.config(function ($routeProvider, $locationProvider) {
         })
         .when("/clientes/editar", {
             templateUrl: "administracion/clientes/captura/capturaCliente.html"
+        })
+        //TRANSPORTISTAS
+        .when("/transportistas/listado", {
+            templateUrl: "administracion/transportistas/listado/listadoTransportistas.html"
+        })
+        .when("/transportistas/nuevo", {
+            templateUrl: "administracion/transportistas/captura/capturaTransportista.html"
+        })
+        .when("/transportistas/editar", {
+            templateUrl: "administracion/transportistas/captura/capturaTransportista.html"
+        })
+        //CHOFERES
+        .when("/choferes/listado", {
+            templateUrl: "administracion/choferes/listado/listadoChoferes.html"
+        })
+        .when("/choferes/nuevo", {
+            templateUrl: "administracion/choferes/captura/capturaChofer.html"
+        })
+        .when("/choferes/editar", {
+            templateUrl: "administracion/choferes/captura/capturaChofer.html"
+        })
+        //TRACTORES
+        .when("/tractores/listado", {
+            templateUrl: "administracion/tractores/listado/listadoTractores.html"
+        })
+        .when("/tractores/nuevo", {
+            templateUrl: "administracion/tractores/captura/capturaTractor.html"
+        })
+        .when("/tractores/editar", {
+            templateUrl: "administracion/tractores/captura/capturaTractor.html"
+        })
+        //REMOLQUES
+        .when("/remolques/listado", {
+            templateUrl: "administracion/remolques/listado/listadoRemolques.html"
+        })
+        .when("/remolques/nuevo", {
+            templateUrl: "administracion/remolques/captura/capturaRemolque.html"
+        })
+        .when("/remolques/editar", {
+            templateUrl: "administracion/remolques/captura/capturaRemolque.html"
+        })
+        //PERFIL
+        .when("/perfil", {
+            templateUrl: "cuenta/perfil/perfil.html"
+        })
+        //AGENDA
+        .when("/fletes/agenda", {
+            templateUrl: "fletes/agenda/agenda.html"
+        })
+        .when("/fletes/nuevo", {
+            templateUrl: "fletes/captura/capturaFlete.html"
         });
 });
