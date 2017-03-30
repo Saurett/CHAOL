b9625767c86cb131e5e78ad600ea5829178cbdb5
Saurett <javiersaurett@gmail.com>
2017-03-30 15:13:18 -0600

    Android > Versión 1.0.20.5
    - Se agregan picker para fecha y hora

--- movil/android/Chaol/app/src/main/java/com/indev/chaol/fragments/FletesDatosGeneralesFragment.java
+++ movil/android/Chaol/app/src/main/java/com/indev/chaol/fragments/FletesDatosGeneralesFragment.java
@@ -1,5 +1,7 @@
 package com.indev.chaol.fragments;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -9,12 +11,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TimePicker;
 
 import com.indev.chaol.R;
 import com.indev.chaol.models.DecodeExtraParams;
 import com.indev.chaol.models.Fletes;
 import com.indev.chaol.utils.Constants;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 
 /**
@@ -25,6 +34,10 @@ public class FletesDatosGeneralesFragment extends Fragment implements View.OnCli
 
     private Button btnTitulo;
     private LinearLayout linearLayout;
+    private EditText txtFechaSalida, txtHoraSalida;
+    private Calendar myCalendar = Calendar.getInstance();
+    private DatePickerDialog.OnDateSetListener date;
+    private TimePickerDialog.OnTimeSetListener time;
 
     private static DecodeExtraParams _MAIN_DECODE = new DecodeExtraParams();
 
@@ -35,10 +48,35 @@ public class FletesDatosGeneralesFragment extends Fragment implements View.OnCli
         btnTitulo = (Button) view.findViewById(R.id.btn_datos_generales_fletes);
         linearLayout = (LinearLayout) view.findViewById(R.id.datos_generales_container);
 
+        txtFechaSalida = (EditText) view.findViewById(R.id.txt_dg_fecha_salida);
+        txtHoraSalida = (EditText) view.findViewById(R.id.txt_dg_hora_salida);
+
         btnTitulo.setOnClickListener(this);
+        txtFechaSalida.setOnClickListener(this);
+        txtHoraSalida.setOnClickListener(this);
+
         linearLayout.setVisibility(View.GONE);
 
         _MAIN_DECODE = (DecodeExtraParams) getActivity().getIntent().getExtras().getSerializable(Constants.KEY_MAIN_DECODE);
+
+        date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateTxtDate();
+            }
+        };
+
+        time = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                myCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                myCalendar.set(Calendar.MINUTE, minute);
+                updateTxtTime();
+            }
+        };
 
         this.onPreRender();
 
@@ -73,6 +111,18 @@ public class FletesDatosGeneralesFragment extends Fragment implements View.OnCli
         }
     }
 
+    private void updateTxtDate() {
+        String myFormat = "dd/MM/yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.ROOT);
+        txtFechaSalida.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    private void updateTxtTime() {
+        String myFormat = "hh:mm a"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.ROOT);
+        txtHoraSalida.setText(sdf.format(myCalendar.getTime()));
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -86,8 +136,19 @@ public class FletesDatosGeneralesFragment extends Fragment implements View.OnCli
 
                 }
                 break;
+            case R.id.txt_dg_fecha_salida:
+                new DatePickerDialog(getContext(), R.style.MyCalendarTheme, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                break;
+            case R.id.txt_dg_hora_salida:
+                new TimePickerDialog(getContext(), R.style.MyCalendarTheme, time, myCalendar
+                        .get(Calendar.HOUR_OF_DAY), myCalendar.get(Calendar.MINUTE),
+                        false).show();
+                break;
         }
     }
+
 
     private void showQuestion() {
         AlertDialog.Builder ad = new AlertDialog.Builder(getContext());
